5083b8631306b603381dab9b162451c2943fbcae
Thomas <oumar.thomas1998@gmail.com>
2019-01-30 12:18:20 +0100

    Adding creator field

--- routes/trips.js
+++ routes/trips.js
@@ -35,8 +35,8 @@ module.exports = app => {
 	});
 
 	app.post('/trips', (req, res) => {
-		const { description, destinationCity, destinationCountry, carryWeight, carryMaxAmount, carryTaxe } = req.body;
-		const trip = new Trip({ description, destinationCity, destinationCountry, carryWeight, carryMaxAmount, carryTaxe });
+		const { description, destinationCity, destinationCountry, carryWeight, carryMaxAmount, carryTaxe, creator } = req.body;
+		const trip = new Trip({ description, destinationCity, destinationCountry, carryWeight, carryMaxAmount, carryTaxe, creator });
 		trip.save().then(
 			() => {
 				res.status(201).send(trip);

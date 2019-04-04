684c94b9f4071d544ec7f0ef97c0d6eaf7629e8a
Thomas Oumar <oumar.thomas1998@gmail.com>
2019-04-04 13:19:29 +0200

    Fixing database URL

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	ENV: process.env.NODE_ENV || 'development',
 	PORT: process.env.PORT || 3000,
 	URL: process.env.BASE_URL || 'https://karry-dev.herokuapp.com/',
-	MONGODB_URI: process.env.MONGODB_URI || 'mongodb://karry-admin:karryproject2019@ds125422.mlab.com:25422/karry_api',
+	MONGODB_URI: process.env.MONGODB_URI || 'mongodb://karry:karryproject2019@ds213615.mlab.com:13615/karry_api',
 	JWT_SECRET: process.env.JWT_SECRET || 'secret',
 	ADMIN_TOKEN: 'hmmmmmmchiwawa',
 	SEND_GRID_API_KEY: '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',

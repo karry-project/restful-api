06d57b74a3860987c70979b80ee4c721fe3d7e04
Nicolas <nicolas.leroy@hetic.net>
2019-05-16 11:17:32 +0200

    Create PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'node index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '51.83.78.229',
+      ref  : 'origin/master',
+      repo : 'git@github.com:karry-project/restful-api.git',
+      path : '/var/www/restful-api',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};

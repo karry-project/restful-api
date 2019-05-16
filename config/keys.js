f3f90049de9119bd1e29d0e2a75653462f15d382
Nicolas <nicolas@eccmperformance.com>
2019-05-16 10:59:33 +0200

    Change baseUrl key

--- config/keys.js
+++ config/keys.js
@@ -1,25 +1,25 @@
 const keys = {
-    app: {
-        baseUrl: 'karry-api-thomasoumar.c9users.io',
-        defaultPort: 3000,
-        databaseUrl: 'mongodb://karry:karryproject2019@ds213615.mlab.com:13615/karry_api',
-        jwt: {
-            secret: 'secret'
-        },
-        acceptedTokens: [
-            'nF0KAGnAIHXly5A8NHz0ax2MugL85ySm',
-            '<user_token>'
-        ]
+  app: {
+    baseUrl: "api.karry.fr",
+    defaultPort: 3000,
+    databaseUrl:
+      "mongodb://karry:karryproject2019@ds213615.mlab.com:13615/karry_api",
+    jwt: {
+      secret: "secret"
     },
-    googleAuth: {
-        clientId: '816322250968-fj28tpsmh35fi7n208sahsc65d1ipe5r.apps.googleusercontent.com'
-    },
-    firebase: {
-        adminKey: 'firebase-adminsdk-85efj@karry-app-f106e.iam.gserviceaccount.com'
-    },
-    sendGrid: {
-        apiKey: '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'
-    }
+    acceptedTokens: ["nF0KAGnAIHXly5A8NHz0ax2MugL85ySm", "<user_token>"]
+  },
+  googleAuth: {
+    clientId:
+      "816322250968-fj28tpsmh35fi7n208sahsc65d1ipe5r.apps.googleusercontent.com"
+  },
+  firebase: {
+    adminKey: "firebase-adminsdk-85efj@karry-app-f106e.iam.gserviceaccount.com"
+  },
+  sendGrid: {
+    apiKey:
+      "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"
+  }
 };
 
 module.exports = keys;

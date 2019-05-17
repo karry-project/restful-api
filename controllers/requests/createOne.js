14754ac4ddf1bbb91b5c638e965d09aa5f6d80ce
ThomasOumar <oumar.thomas1998@gmail.com>
2019-05-17 16:29:08 +0200

    Preparing for chat feature

--- controllers/requests/createOne.js
+++ controllers/requests/createOne.js
@@ -1,10 +1,35 @@
 const Request = require('./../../models/Request');
+const Room = require('./../../models/Room');
 
 module.exports = (req, res) => {
-    const request = new Request(req.body);
-    request.save().then(
-		() => {
+	const request = new Request(req.body);
+	request.save().then(
+		request => {
 			res.status(201).send(request);
+			/*
+							Trip.findOne({ _id: request.tripId }).then(
+								trip => {
+									const room = Room({
+										userId: req.user._id,
+										senderId: trip.owner,
+										requestId: request._id,
+										messages: []
+									});
+									room.save().then(
+										(room) => {
+										
+										},
+										err => {
+											res.status(400).send(err);
+										});
+									res.status(201).send(request);
+								},
+								() => {
+									
+								}
+							)
+						*/
+
 		},
 		err => {
 			res.status(400).send(err);

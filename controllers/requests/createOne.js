4d941f0c83c21ced0f454bf9f95fb1763627079e
ThomasOumar <oumar.thomas1998@gmail.com>
2019-05-26 17:39:06 +0200

    Embed user in trip object

--- controllers/requests/createOne.js
+++ controllers/requests/createOne.js
@@ -1,35 +1,24 @@
 const Request = require('./../../models/Request');
 const Room = require('./../../models/Room');
+const Trip = require('./../../models/Trip');
 
 module.exports = (req, res) => {
 	const request = new Request(req.body);
 	request.save().then(
 		request => {
-			res.status(201).send(request);
-			/*
-							Trip.findOne({ _id: request.tripId }).then(
-								trip => {
-									const room = Room({
-										userId: req.user._id,
-										senderId: trip.owner,
-										requestId: request._id,
-										messages: []
-									});
-									room.save().then(
-										(room) => {
-										
-										},
-										err => {
-											res.status(400).send(err);
-										});
-									res.status(201).send(request);
-								},
-								() => {
-									
-								}
-							)
-						*/
-
+			const room = Room({
+				userId: req.user._id,
+				senderId: trip.owner,
+				requestId: request._id,
+				messages: []
+			});
+			room.save().then(
+				(room) => {
+					console.log(' create room with id ' + room.id + 'for trip ' + trip.id + ' such as request equal' + request);
+				},
+				err => {
+					res.status(400).send(err);
+				});
 		},
 		err => {
 			res.status(400).send(err);

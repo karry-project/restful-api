44b14ff443f03dbdc53230d746fa4079dac83eeb
ThomasOumar <oumar.thomas1998@gmail.com>
2019-05-26 18:11:38 +0200

    Embedding user in search list trips

--- controllers/requests/createOne.js
+++ controllers/requests/createOne.js
@@ -6,19 +6,23 @@ module.exports = (req, res) => {
 	const request = new Request(req.body);
 	request.save().then(
 		request => {
-			const room = Room({
-				userId: req.user._id,
-				senderId: trip.owner,
-				requestId: request._id,
-				messages: []
-			});
-			room.save().then(
-				(room) => {
-					console.log(' create room with id ' + room.id + 'for trip ' + trip.id + ' such as request equal' + request);
+			Trip.findOne({ _id: request.tripId }).then(
+				trip => {
+					const room = Room({
+						userId: req.user._id,
+						senderId: trip.owner,
+						requestId: request._id,
+						messages: []
+					});
+					room.save().then(
+						(room) => {
+							res.status(201).send(request);
+						},
+						err => {
+							res.status(400).send(err);
+						});
 				},
-				err => {
-					res.status(400).send(err);
-				});
+				() => { })
 		},
 		err => {
 			res.status(400).send(err);
--- controllers/trips/findSearch.js
+++ controllers/trips/findSearch.js
@@ -3,7 +3,7 @@ const Trip = require('./../../models/Trip');
 module.exports = (req, res) => {
 	const searchObject = req.query;
 
-	Trip.find(searchObject).then(
+	Trip.find(searchObject).populate('requestList').populate('owner').populate('joinList').then(
 		trips => {
 			res.status(200).send(trips);
 		},
